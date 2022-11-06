4aa266035bc0beeb184731eecee3198060332f6d
AutomationTest001 <automationtest307@gmail.com>
2022-11-06 10:23:26 +0530

    QA1 - Added samplecode.java file code for Hello World

---
+++ src/test/java/com/itview/basictestcases/selenium_testng_project/SampleCode.java
@@ -0,0 +1,11 @@
+package com.itview.basictestcases.selenium_testng_project;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World !!!");
+
+	}
+
+}
